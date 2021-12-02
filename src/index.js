a074431b7f594e5ca994cf25f12d2e86cef9cf89
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2021-12-02 17:06:29 -0300

    WIP

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
 import App from './App';
 import About from './About';
 import Contact from './Contact';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import getUser from './Utils/Common';
 
 const routing = (
   <BrowserRouter>
     <div>
-      <h3>Clue Mediator</h3>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+	 <Routes>
+        	<Route path="/" element={<App />} />
+        	<Route path="/about" element={<About />} />
+        	<Route path="/contact" element={<Contact />} />
+		<Route path="/login" element={<Login />} />
+		<Route path="/dashboard" element={
+			<RequireAuth redirectTo="/login">
+				<Dashboard />
+			</RequireAuth>
+		} />
+      	  </Routes>
     </div>
   </BrowserRouter>
 )
+
+function RequireAuth({ children, redirectTo }) {
+  let isAuthenticated = getUser();
+  return isAuthenticated ? children : <Navigate to={redirectTo} />;
+}
 
 ReactDOM.render(routing, document.getElementById('root'));
