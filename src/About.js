7bc3d12e2f77b88c4879472bb51036a3d7da645e
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2021-12-09 10:52:39 -0300

    Changing from classes to functions

--- src/About.js
+++ src/About.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 
-class About extends Component {
-  render() {
-    return <div>
-      <h4>About</h4>
-      <p>This is a challenge for Assembler School.</p>
-    </div>
-  }
+function About(props) {
+  return <div>
+    <h4>About</h4>
+    <p>This is a challenge for Assembler School.</p>
+  </div>
 }
 
 export default About;
--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 
-class Contact extends Component {
-  render() {
-    return <div>
-      <h4>Contact</h4>
-      <p>Email me at <a href="mailto:mauro.chojrin@leewayweb.com">mauro.chojrin@leewayweb.com</a></p>
-    </div>
-  }
+function Contact(props) {
+  return <div>
+    <h4>Contact</h4>
+    <p>Email me at <a href="mailto:mauro.chojrin@leewayweb.com">mauro.chojrin@leewayweb.com</a></p>
+  </div>
 }
 
 export default Contact;
